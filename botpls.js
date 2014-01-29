c7b7257a09b59b9c86ca0da36e093ac5f82cd539
Bodrew <bodrew12@gmail.com>
2014-01-29 10:09:28 -0500

    k
    kkk

---
+++ botpls.js
@@ -0,0 +1,23 @@
+API.on(API.CHAT, callback);
+function callback(data) {
+  if (data.message === ".bod") {
+  API.sendChat('Present!');
+  } 
+  else {}
+  if (data.message === (".smodlovesbod")){
+  API.sendChat("It's true, I heard @Smodoopa say it!");
+  }
+  else {}
+  if (data.message === (".foodfucksdrew")){
+  API.sendChat("*moans*")
+  }
+  else{}
+  if (data.message === (".wakeup")){
+  API.sendChat("@Bodrew Wake up, me!")
+  }
+  else {}
+  if (data.message === (".bodhartsbeany")){
+  API.sendChat("And she still won't date me :expressionless:")
+  }
+  else{}
+}
