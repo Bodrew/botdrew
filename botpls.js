9225ee778af3b4bbee20256e2c951cfd59ad5d6f
Bodrew <bodrew12@gmail.com>
2014-01-29 00:44:34 -0500

    Update botpls.js

--- botpls.js
+++ botpls.js
@@ -1,23 +1,31 @@
+//Copy this and paste in the Plug Console: javascript:$.getScript('https://github.com/Bodrew/botdrew/edit/botpls/botpls.js');
 API.on(API.CHAT, callback);
 function callback(data) {
+	//Simple bot command
   if (data.message === ".bod") {
   API.sendChat('Present!');
   } 
   else {}
+	//Expressing my love for Justin
   if (data.message === (".smodlovesbod")){
   API.sendChat("It's true, I heard @Smodoopa say it!");
   }
   else {}
-  if (data.message === (".foodfucksdrew")){
-  API.sendChat("*moans*")
-  }
-  else{}
+	//The wake-up command
   if (data.message === (".wakeup")){
   API.sendChat("@Bodrew Wake up, me!")
   }
   else {}
+	//Expressing my love for Miss Bean
   if (data.message === (".bodhartsbeany")){
   API.sendChat("And she still won't date me :expressionless:")
   }
   else{}
+	//Execute to turn up! \o/
+  if (data.messgae === (".rave")){
+  API.sendChat("@" + data.from + " gettin' turnt up!")
+  API.setVolume(30)}
+  else{}
+  
+  //New commands right above here!
 }
