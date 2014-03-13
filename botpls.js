e8924e294643dbde6341a4498171888a8a45ca60
Bodrew <bodrew12@gmail.com>
2014-03-13 19:12:58 -0400

    Update botpls.js
    Added confirmation load?

--- botpls.js
+++ botpls.js
@@ -1,3 +1,4 @@
+onLoad(alert("BodBot successfully loaded!"));
 //GO HERE TO EDIT THE BOT, THEN COPY PASTE IT BACK HERE: https://github.com/Bodrew/botdrew/edit/botpls/botpls.js
 //Copy this and paste in the Plug Console: javascript:$.getScript('https://github.com/Bodrew/botdrew/edit/botpls/botpls.js');
 API.on(API.CHAT, callback);
