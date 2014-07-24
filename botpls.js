d4ae1c70347c773c77496ac5d404e27828bb032a
Bodrew <bodrew12@gmail.com>
2014-07-24 13:25:18 -0400

    Update botpls.js

--- botpls.js
+++ botpls.js
@@ -1,21 +1,15 @@
 //GO HERE TO EDIT THE BOT, THEN COPY PASTE IT BACK HERE: https://github.com/Bodrew/botdrew/edit/botpls/botpls.js
 //Copy this and paste in the Plug Console: javascript:$.getScript('https://github.com/Bodrew/botdrew/edit/botpls/botpls.js');
-API.on(API.CHAT, msg);
-function msg(data) {
+API.on(API.CHAT, callback);
+function callback(data) {
   //Simple bot command
   if (data.message === (".bod")) {
   API.sendChat('Present!');
   } 
   else {}
-  //Expressing my love for Justin
-  //Execute to turn up! \o/
-  //if (data.messgae === (".rave")){
-  //API.sendChat(data.from + " gettin' turnt up!")
-  //}
-  //else{}
-  //Beany's insult command ;3
   
   if (data.message === ("TIME FOR A LOTTERY! I WILL CHOOSE A WINNER IN 5 MINUTES! Type '.a' in the chat to enter!")){
     API.sendChat('.a');
   }
+  else {}
 }
